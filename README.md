d6ea42169d9b574431daa49fb7303eace916d623
Christof Pieloth <pieloth@labp.htwk-leipzig.de>
2014-12-12 10:03:55 +0100

    [CHANGE] Use CMake, add AUTHORS and license header.

--- README.md
+++ README.md
@@ -4,3 +4,14 @@ EEG Sensor Generator
 This tool generates virtual EEG sensor positions out of a BEM layer.
 These positions can be used to compute a high resolution leadfield.
 This leadfield can be used for a leadfield interpolation.
+
+WWW: http://www.labp.htwk-leipzig.de   
+SRC: https://bitbucket.org/labp   
+
+
+Compile
+-------
+
+    cd build
+    cmake ../src
+    make
--- src/eeg_sensor_generator.cpp
+++ src/eeg_sensor_generator.cpp
@@ -1,3 +1,26 @@
+//---------------------------------------------------------------------------
+//
+// Project: NA-Online ( http://www.labp.htwk-leipzig.de )
+//
+// Copyright 2010 Laboratory for Biosignal Processing, HTWK Leipzig, Germany
+//
+// This file is part of NA-Online.
+//
+// NA-Online is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// NA-Online is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with NA-Online. If not, see <http://www.gnu.org/licenses/>.
+//
+//---------------------------------------------------------------------------
+
 #include <cstdlib>
 #include <iostream>
 #include <limits>
